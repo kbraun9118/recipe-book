fdc642383dfd22c99b4b80d602e5deced02602e8
Kurt Braun <kbraun9118@gmail.com>
2023-08-10 14:32:45 -0500

    moved migrate to build time

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,10 @@
 import { env } from '$env/dynamic/private';
+import { DATABASE_URL } from '$env/static/private';
 import { drizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres';
 import pg from 'pg';
 import { schema } from './schema';
+import { building } from '$app/environment';
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
 
 export type DbClient = NodePgDatabase<typeof schema>;
 
@@ -10,12 +13,17 @@ const globalForDrizzle = globalThis as unknown as {
 };
 
 export const dbClient = new pg.Client({
-  connectionString: env.DATABASE_URL || '',
+  connectionString: env.DATABASE_URL || DATABASE_URL,
 });
 
 const isProd = env.NODE_ENV === 'production';
 
 const db = globalForDrizzle.db ?? drizzle(dbClient, { schema, logger: !isProd });
+
+if (building) {
+  await dbClient.connect();
+  await migrate(db, { migrationsFolder: 'migrations' });
+}
 
 if (!isProd) globalForDrizzle.db = db;
 
