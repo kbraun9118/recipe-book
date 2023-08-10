432b176d05ff27c544c0a7b777f1178b6ff0fcf2
Kurt Braun <kbraun9118@gmail.com>
2023-08-10 15:05:56 -0500

    pull out migrations from hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,14 +2,9 @@ import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
 import db from '$lib/server/db';
 import { seedDB } from '$lib/server/db/seed';
-import { migrate } from 'drizzle-orm/node-postgres/migrator';
 
-if (!building) {
-  await migrate(db, { migrationsFolder: 'migrations' });
-
-  if (env.NODE_ENV !== 'production') {
-    await seedDB(db);
-  }
+if (!building && env.NODE_ENV !== 'production') {
+  await seedDB(db);
 }
 
 if (!env.PAGE_LOGIN) {
