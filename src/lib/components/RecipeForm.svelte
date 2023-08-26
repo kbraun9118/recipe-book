3cdfeb574e37a34d07ce76ea6b7b4f456cbaeb93
Kurt Braun <kbraun@kbraun.dev>
2023-08-26 17:33:02 -0500

    fix inifinite loop

--- src/lib/components/RecipeForm.svelte
+++ src/lib/components/RecipeForm.svelte
@@ -14,14 +14,14 @@
 
   const addIngredient = () => {
     form.update((f) => {
-      f.ingredients.push({ name: '', unit: 'cups', amount: 0 });
+      f.ingredients?.push({ name: '', unit: 'cups', amount: 0 });
       return f;
     });
   };
 
   const removeIngredient = (index: number) => () => {
     form.update((f) => {
-      f.ingredients = f.ingredients.filter((_, i) => i !== index);
+      f.ingredients = f.ingredients?.filter((_, i) => i !== index);
       return f;
     });
   };
@@ -36,7 +36,9 @@
   }));
 
   function onInputChipSelect(event: CustomEvent<AutocompleteOption>): void {
-    $form.tags = [...$form.tags, event.detail.value as string];
+    $form.tags = $form.tags
+      ? [...$form.tags, event.detail.value as string]
+      : [event.detail.value as string];
     inputChip = '';
   }
 </script>
@@ -76,83 +78,85 @@
         <ErrorText fieldName="notes" text={$errors.notes} />
       </div>
       <div>
-        {#key $form.tags}
-          <!-- svelte-ignore a11y-label-has-associated-control -->
-          <label class="label">
-            Tags*
-            <div
-              use:popup={{
-                event: 'focus-click',
-                target: 'inputChipAutocomplete',
-                placement: 'bottom-start',
-              }}>
-              <InputChip
-                bind:input={inputChip}
-                bind:value={$form.tags}
-                name="tags"
-                placeholder=""
-                class={$errors.tags ? 'input-error' : null} />
-            </div>
-            <div
-              class="card w-full max-w-sm max-h-48 p-4 overflow-y-auto"
-              tabindex="-1"
-              data-popup="inputChipAutocomplete">
-              <Autocomplete
-                bind:input={inputChip}
-                options={inputChipAutocompleteOptions}
-                denylist={$form.tags}
-                on:selection={onInputChipSelect} />
-            </div>
-          </label>
-          <ErrorText fieldName="tags" text={$errors.tags?._errors} />
-        {/key}
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label class="label">
+          Tags*
+          <div
+            use:popup={{
+              event: 'focus-click',
+              target: 'inputChipAutocomplete',
+              placement: 'bottom-start',
+            }}>
+            <InputChip
+              bind:input={inputChip}
+              bind:value={$form.tags}
+              name="tags"
+              placeholder=""
+              class={$errors.tags ? 'input-error' : null} />
+          </div>
+          <div
+            class="card w-full max-w-sm max-h-48 p-4 overflow-y-auto"
+            tabindex="-1"
+            data-popup="inputChipAutocomplete">
+            <Autocomplete
+              bind:input={inputChip}
+              options={inputChipAutocompleteOptions}
+              denylist={$form.tags}
+              on:selection={onInputChipSelect} />
+          </div>
+        </label>
+        <ErrorText fieldName="tags" text={$errors.tags?._errors} />
       </div>
       <div>
         <div class="label">Ingredients*</div>
         <ul class="space-y-2">
-          {#each $form.ingredients as _, i}
-            <li>
-              <div>
-                <div class="flex gap-2">
-                  <div
-                    class="input-group input-group-divider grid-cols-[minmax(100px,_2fr)_minmax(50px,_1fr)_minmax(50px,_1fr)]"
-                    class:input-error={$errors.ingredients?.[i]?.name ||
-                      $errors.ingredients?.[i]?.amount}>
-                    <label hidden for={`ingredientname${i}`}>
-                      Ingredient {i} Name
-                    </label>
-                    <input
-                      class="capitalize"
-                      id={`ingredientname${i}`}
-                      bind:value={$form.ingredients[i].name} />
-                    <label hidden for={`ingredientamount${i}`}>
-                      Ingredient {i} Amount
-                    </label>
-                    <input
-                      id={`ingredientamount${i}`}
-                      type="number"
-                      step="any"
-                      bind:value={$form.ingredients[i].amount} />
-                    <label for={`ingredientunit${i}`} hidden>Ingredient {i} Unit</label>
-                    <IngredientUnitSelect
-                      id={`ingredientunit${i}`}
-                      bind:value={$form.ingredients[i].unit}
-                      isInput={false} />
+          {#if $form.ingredients}
+            {#each $form.ingredients as _, i}
+              <li>
+                <div>
+                  <div class="flex gap-2">
+                    <div
+                      class="input-group input-group-divider grid-cols-[minmax(100px,_2fr)_minmax(50px,_1fr)_minmax(50px,_1fr)]"
+                      class:input-error={$errors.ingredients?.[i]?.name ||
+                        $errors.ingredients?.[i]?.amount}>
+                      <label hidden for={`ingredientname${i}`}>
+                        Ingredient {i} Name
+                      </label>
+                      <input
+                        class="capitalize"
+                        id={`ingredientname${i}`}
+                        bind:value={$form.ingredients[i].name} />
+                      <label hidden for={`ingredientamount${i}`}>
+                        Ingredient {i} Amount
+                      </label>
+                      <input
+                        id={`ingredientamount${i}`}
+                        type="number"
+                        step="any"
+                        bind:value={$form.ingredients[i].amount} />
+                      <label for={`ingredientunit${i}`} hidden>Ingredient {i} Unit</label>
+                      <IngredientUnitSelect
+                        id={`ingredientunit${i}`}
+                        bind:value={$form.ingredients[i].unit}
+                        isInput={false} />
+                    </div>
+                    {#if $form.ingredients.length > 1}
+                      <button
+                        class="btn variant-outline-error font-bold hover:variant-filled-error w-12"
+                        type="button"
+                        on:click={removeIngredient(i)}
+                        >-
+                      </button>
+                    {/if}
                   </div>
-                  {#if $form.ingredients.length > 1}
-                    <button
-                      class="btn variant-outline-error font-bold hover:variant-filled-error w-12"
-                      type="button"
-                      on:click={removeIngredient(i)}
-                      >-
-                    </button>
-                  {/if}
+                  <ErrorText fieldName="ingredient name" text={$errors.ingredients?.[i]?.name} />
+                  <ErrorText
+                    fieldName="ingredient amount"
+                    text={$errors.ingredients?.[i]?.amount} />
                 </div>
-                <ErrorText fieldName="ingredient name" text={$errors.ingredients?.[i]?.name} />
-                <ErrorText fieldName="ingredient amount" text={$errors.ingredients?.[i]?.amount} />
-              </div>
-            </li>
-          {/each}
+              </li>
+            {/each}
+          {/if}
         </ul>
         <div>
           <button
