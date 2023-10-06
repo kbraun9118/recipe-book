5e7528db9c7787708dbba60678f286a2dcff324c
Kurt Braun <kbraun@shelterinsurance.com>
2023-10-06 09:56:47 -0500

    added a progress bar for loading

--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -2,15 +2,24 @@
   import { enhance } from '$app/forms';
   import PageTransition from '$lib/components/PageTransition.svelte';
   import { faBars } from '@fortawesome/free-solid-svg-icons';
-  import { AppBar, AppShell, popup } from '@skeletonlabs/skeleton';
+  import { AppBar, AppShell, ProgressBar, popup } from '@skeletonlabs/skeleton';
   import Fa from 'svelte-fa';
   import type { LayoutData } from './$types';
+  import { navigating } from '$app/stores';
 
   export let data: LayoutData;
+
+  $: navigationValue = !$navigating ? 0 : undefined;
 </script>
 
 <AppShell>
   <svelte:fragment slot="header">
+    <ProgressBar
+      rounded="rounded-none"
+      height="h-1"
+      track="bg-surface-100-800-token"
+      meter="variant-filled-primary"
+      value={navigationValue} />
     <AppBar gridColumns="grid-cols-2" slotTrail="place-content-end">
       <a class="font-bold text-xl" href="/">Recipe Book</a>
       <svelte:fragment slot="trail">
