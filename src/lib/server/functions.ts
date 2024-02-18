0d6ea9de1eb1037cdc383e94ed3d5223bda1e1ad
Kurt Braun <kbraun@kbraun.dev>
2024-02-18 10:09:13 -0600

    Add transaction typing

--- src/lib/server/functions.ts
+++ src/lib/server/functions.ts
@@ -1,12 +1,22 @@
-import { and, eq, inArray } from 'drizzle-orm';
+import { and, eq, inArray, type ExtractTablesWithRelations } from 'drizzle-orm';
 import { ingredients, recipesIngredients, recipesTags, tags } from './db/schema/recipe';
 import db from './db';
+import type { PgTransaction } from 'drizzle-orm/pg-core';
+import type { PostgresJsQueryResultHKT } from 'drizzle-orm/postgres-js';
+import type { schema } from './db/schema';
+
+type DbTransaction = PgTransaction<
+  PostgresJsQueryResultHKT,
+  typeof schema,
+  ExtractTablesWithRelations<typeof schema>
+>;
 
 export async function addIngredient(
   recipeId: number,
   ingredient: { name: string; amount: number; unit: string },
 ) {
   const query = and(eq(ingredients.name, ingredient.name), eq(ingredients.unit, ingredient.unit));
+  db.transaction;
 
   await db
     .insert(ingredients)
